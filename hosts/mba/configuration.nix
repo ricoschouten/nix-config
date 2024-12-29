1ab516e6b33fa34b3dcdb650930c498d5497b146
Rico Schouten <ricoschouten@gmail.com>
2024-12-29 15:21:04 +0100

    Disable unused Nix Darwin configuration

--- hosts/mba/configuration.nix
+++ hosts/mba/configuration.nix
@@ -49,117 +49,92 @@
       };
       NSGlobalDomain = {
         AppleInterfaceStyle = "Dark";
-        ApplePressAndHoldEnabled = false;
-        AppleShowAllExtensions = true;
-        KeyRepeat = 2;
+        # ApplePressAndHoldEnabled = false;
+        # AppleShowAllExtensions = true;
+        # KeyRepeat = 2;
         NSAutomaticCapitalizationEnabled = false;
         NSAutomaticDashSubstitutionEnabled = false;
         NSAutomaticQuoteSubstitutionEnabled = false;
         NSAutomaticSpellingCorrectionEnabled = false;
-        NSAutomaticWindowAnimationsEnabled = false;
-        NSDocumentSaveNewDocumentsToCloud = false;
-        NSNavPanelExpandedStateForSaveMode = true;
-        PMPrintingExpandedStateForPrint = true;
+        # NSAutomaticWindowAnimationsEnabled = false;
+        # NSDocumentSaveNewDocumentsToCloud = false;
+        # NSNavPanelExpandedStateForSaveMode = true;
+        # PMPrintingExpandedStateForPrint = true;
       };
-      LaunchServices = {
-        LSQuarantine = false;
-      };
+      # LaunchServices = {
+      #   LSQuarantine = false;
+      # };
       trackpad = {
         TrackpadRightClick = true;
         TrackpadThreeFingerDrag = true;
         Clicking = true;
       };
       finder = {
-        AppleShowAllFiles = true;
-        CreateDesktop = false;
-        FXDefaultSearchScope = "SCcf";
-        FXEnableExtensionChangeWarning = false;
-        FXPreferredViewStyle = "Nlsv";
-        QuitMenuItem = true;
+        # AppleShowAllFiles = true;
+        # CreateDesktop = false;
+        # FXDefaultSearchScope = "SCcf";
+        # FXEnableExtensionChangeWarning = false;
+        # FXPreferredViewStyle = "Nlsv";
+        # QuitMenuItem = true;
         ShowPathbar = true;
-        ShowStatusBar = true;
-        _FXShowPosixPathInTitle = true;
-        _FXSortFoldersFirst = true;
+        # ShowStatusBar = true;
+        # _FXShowPosixPathInTitle = true;
+        # _FXSortFoldersFirst = true;
       };
       dock = {
         autohide = true;
-        expose-animation-duration = 0.15;
-        show-recents = false;
-        showhidden = true;
-        persistent-apps = [
-          "/Applications/Brave Browser.app"
-          "${pkgs.alacritty}/Applications/Alacritty.app"
-          "${pkgs.telegram-desktop}/Applications/Telegram.app"
-        ];
-        tilesize = 30;
-        wvous-bl-corner = 1;
-        wvous-br-corner = 1;
-        wvous-tl-corner = 1;
-        wvous-tr-corner = 1;
-      };
-      screencapture = {
-        location = "/Users/${userConfig.name}/Downloads/temp";
-        type = "png";
-        disable-shadow = true;
+        # expose-animation-duration = 0.15;
+        # show-recents = false;
+        # showhidden = true;
+        # persistent-apps = [
+        #   "/Applications/Brave Browser.app"
+        #   "${pkgs.alacritty}/Applications/Alacritty.app"
+        #   "${pkgs.telegram-desktop}/Applications/Telegram.app"
+        # ];
+        # tilesize = 30;
+        # wvous-bl-corner = 1;
+        # wvous-br-corner = 1;
+        # wvous-tl-corner = 1;
+        # wvous-tr-corner = 1;
       };
     };
-    keyboard = {
-      enableKeyMapping = true;
-      # swapLeftCtrlAndFn = true;
-      # Remap §± to ~
-      userKeyMapping = [
-        {
-          HIDKeyboardModifierMappingDst = 30064771125;
-          HIDKeyboardModifierMappingSrc = 30064771172;
-        }
-      ];
-    };
+    # keyboard = {
+    #   enableKeyMapping = true;
+    #   # swapLeftCtrlAndFn = true;
+    #   # Remap §± to ~
+    #   userKeyMapping = [
+    #     {
+    #       HIDKeyboardModifierMappingDst = 30064771125;
+    #       HIDKeyboardModifierMappingSrc = 30064771172;
+    #     }
+    #   ];
+    # };
   };
 
   # System packages
   environment.systemPackages = with pkgs; [
-    (python3.withPackages (ps: with ps; [pip virtualenv]))
-    awscli2
-    colima
-    delta
     docker
     du-dust
     eza
     fd
     jq
-    kubectl
-    lazydocker
     nh
-    openconnect
-    pipenv
-    ripgrep
-    telegram-desktop
-    terraform
-    terragrunt
   ];
 
   # Zsh configuration
   programs.zsh.enable = true;
 
   # Fonts configuration
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.meslo-lg
-    roboto
-  ];
+  # fonts.packages = with pkgs; [
+  #   nerd-fonts.jetbrains-mono
+  #   nerd-fonts.meslo-lg
+  #   roboto
+  # ];
 
   homebrew = {
     enable = true;
     casks = [
       "aerospace"
-      "anki"
-      "brave-browser"
-      "dozer"
-      "obs"
-      "raycast"
-    ];
-    taps = [
-      "nikitabobko/tap"
     ];
     onActivation.cleanup = "zap";
   };
